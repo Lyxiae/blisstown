19b6f8803a5c5bda544385dcf040a0c89f8eb31b
Lyxiae <43345211+Lyxiae@users.noreply.github.com>
2023-03-15 11:14:44 +0100

    update styles

--- src/js/scripts/viewtopic_fields.js
+++ src/js/scripts/viewtopic_fields.js
@@ -8,25 +8,38 @@ class ViewtopicFields {
   moveFields() {
     function main() {
       const postList = document.querySelectorAll('.viewtopic__post');
-     
-      postList.forEach(p => {
+
+      postList.forEach((p) => {
         const bioField = p.querySelector('.topic-profile-field-biographie');
         const messagesField = p.querySelector('.topic-profile-field-messages');
-        const availField = p.querySelector('.topic-profile-field-disponibilite');
+        const availField = p.querySelector(
+          '.topic-profile-field-disponibilite'
+        );
         const iconField = p.querySelector('.topic-profile-field-icon');
         const quoteField = p.querySelector('.topic-profile-field-citation');
         const bioTarget = p.querySelector('.viewtopic__bio-container');
-        const messagesTarget = p.querySelector('.viewtopic__messages-container');
+        const messagesTarget = p.querySelector(
+          '.viewtopic__messages-container'
+        );
         const availTarget = p.querySelector('.viewtopic__dispo-container');
         const iconTarget = p.querySelector('.viewtopic__head-icon-container');
         const quoteTarget = p.querySelector('.viewtopic__quote-container');
-  
-        bioTarget.appendChild(bioField);
+
+        if (bioTarget && bioField) {
+          bioTarget.appendChild(bioField);
+        }
+
         messagesTarget.appendChild(messagesField);
-        availTarget.appendChild(availField);
-        iconTarget.appendChild(iconField);
-        quoteTarget.appendChild(quoteField);
-      })
+        if (availTarget && availField) {
+          availTarget.appendChild(availField);
+        }
+        if (iconTarget && iconField) {
+          iconTarget.appendChild(iconField);
+        }
+        if (quoteTarget && quoteField) {
+          quoteTarget.appendChild(quoteField);
+        }
+      });
     }
 
     document.addEventListener('DOMContentLoaded', main);
